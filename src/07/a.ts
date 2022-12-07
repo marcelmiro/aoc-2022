a999b06c7c368ca7be31efd306a511a1444c7bfa
Marcel <marcelmiro1@hotmail.com>
2022-12-07 17:56:08 +0100

    Day 7

---
+++ src/07/a.ts
@@ -0,0 +1,75 @@
+import rawData from './input'
+
+const data = rawData.split('\n')
+
+// `Tree` values can be a dir (subtree) with total size tracker or a file size (number)
+type Dir = { totalSize: number; children: Tree }
+type Tree = { [key: string]: Dir | number }
+
+const tree: Tree = { '/': { totalSize: 0, children: {} } }
+
+const currentPath: string[] = []
+
+const isTree = (tree?: Tree[string]): tree is Dir =>
+	typeof tree === 'object' &&
+	typeof tree.totalSize === 'number' &&
+	typeof tree.children === 'object'
+
+function addDirOrFile(name: string, size: number | null, path: string[]) {
+	const subtree = path.reduce((tree: Tree | undefined, dir) => {
+		const subtree = tree?.[dir]
+		if (!isTree(subtree)) return undefined
+		if (size) subtree.totalSize += size
+		return subtree.children
+	}, tree)
+
+	// Ignore size === 0 as sizes from input file are always > 0
+	if (subtree && !subtree[name])
+		subtree[name] = size || { totalSize: 0, children: {} }
+}
+
+for (const line of data) {
+	// Command
+	if (line.startsWith('$ ')) {
+		if (!line.includes('cd ')) continue
+		const dir = line.substring(line.indexOf('cd ') + 3)
+		if (dir === '..') currentPath.pop()
+		else currentPath.push(dir)
+	}
+
+	// Dir (from `ls` command)
+	if (line.startsWith('dir ')) {
+		const dir = line.substring(line.indexOf('dir ') + 4)
+		if (!dir) continue
+		addDirOrFile(dir, null, currentPath)
+	}
+
+	// File (from `ls` command)
+	const fileRegex = /\d+\s.+/g
+	if (fileRegex.test(line)) {
+		const [size, name] = line.split(' ')
+		addDirOrFile(name, Number(size), currentPath)
+	}
+}
+
+// Get total sizes of all dirs in `tree`
+const dirSizes: number[] = []
+
+function getDirsWithSize(tree: Tree) {
+	for (const name of Object.keys(tree)) {
+		const value = tree[name]
+		if (isTree(value)) {
+			dirSizes.push(value.totalSize)
+			getDirsWithSize(value.children)
+		}
+	}
+}
+
+getDirsWithSize(tree)
+
+// Sum total sizes of dirs with max size 100,000
+const totalSizeOfDirs = dirSizes
+	.filter((size) => size <= 100_000)
+	.reduce((sum, cur) => sum + cur, 0)
+
+console.log(totalSizeOfDirs)
